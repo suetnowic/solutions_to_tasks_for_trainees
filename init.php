da8efc2733cdc2ffaff55850b11339db7ae15189
Viktor Suetnov <57311061+suetnowic@users.noreply.github.com>
2023-10-12 12:03:15 +0300

    Update init.php

--- init.php
+++ init.php
@@ -1,3 +1,24 @@
 <?php
 
-require_once(__DIR__ . '/events.php');
+session_start();
+
+if (isset($_GET['noinit']) && !empty($_GET['noinit']))
+{
+    $strNoInit = strval($_GET['noinit']);
+    if ($strNoInit == 'N')
+    {
+        if (isset($_SESSION['NO_INIT']))
+            unset($_SESSION['NO_INIT']);
+    }
+    elseif ($strNoInit == 'Y')
+    {
+        $_SESSION['NO_INIT'] = 'Y';
+    }
+}
+if (!(isset($_SESSION['NO_INIT']) && $_SESSION['NO_INIT'] == 'Y'))
+{
+    if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/events.php"))
+        require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/events.php");
+}
+
+
