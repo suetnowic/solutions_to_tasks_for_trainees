2a8349bbf7f86becaa026c48a3078d63464158e1
Viktor Suetnov <57311061+suetnowic@users.noreply.github.com>
2023-10-12 12:20:33 +0300

    Update events.php

--- events.php
+++ events.php
@@ -1,8 +1,10 @@
 <?php
 
 use \Bitrix\Main\EventManager;
+use Only\Site\Handlers\Iblock as IblockHandler;
+//use Only\Site\Agents\Iblock as IblockAgent;
 
 $eventManager = EventManager::getInstance();
 
-$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementAdd', ['Only\\Site\\Handlers\\', 'addLog']);
-$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', ['Only\\Site\\Handlers\\', 'addLog']);
+$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementAdd', [IblockHandler::class, 'addLog']);
+$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', [IblockHandler::class, 'addLog']);
