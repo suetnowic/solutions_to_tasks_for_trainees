08f4d8a9416375aa4350994ec6f027467febce57
Viktor Suetnov <57311061+suetnowic@users.noreply.github.com>
2023-10-15 11:54:14 +0300

    Update events.php

--- events.php
+++ events.php
@@ -1,9 +1,5 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/modules/dev.site/lib/Handlers/Iblock.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/modules/dev.site/lib/Agents/Iblock.php');
-
 use Bitrix\Main\EventManager;
 use Only\Site\Handlers\Iblock as IblockHandler;
 use Only\Site\Agents\Iblock as IblockAgent;
